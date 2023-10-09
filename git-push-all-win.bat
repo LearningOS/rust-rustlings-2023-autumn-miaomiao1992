78f518f29d61679fa0e7476b5b26398fc96df3a3
miaomiao1992 <644284807@qq.com>
2023-10-09 18:38:40 +0800

    2023-10-09-18:38:39-push-miaomiao1992-

---
+++ git-push-all-win.bat
@@ -0,0 +1,16 @@
+@echo off
+set /p input=please enter the comments: 
+
+set "Time=%time:~0,8%"
+set "t=%Time: =0%"
+
+set "remarks=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push-miaomiao1992-%input%"
+echo %remarks%
+
+git add .
+
+git commit -m  %remarks%
+git push origin main
+
+echo --End--
+@pause
